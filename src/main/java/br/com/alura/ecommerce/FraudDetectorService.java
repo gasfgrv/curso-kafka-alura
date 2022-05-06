7fb71958e4b9b83a491b4b34b1350aaac9c15cc6
Gustavo Silva <gustavo_almeida11@hotmail.com>
2022-05-05 22:42:07 -0300

    Max poll e dando mais chances para auto commit

--- src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
+++ src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.UUID;
 
 public class FraudDetectorService {
 
@@ -42,6 +43,8 @@ public class FraudDetectorService {
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
+        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, FraudDetectorService.class.getSimpleName() + UUID.randomUUID());
+        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1"); // Recomeda-se essa configuração, para que o consumer trabalhe de um registro por vez mandando a confirmação de leitura da mensagem, para evitar algum problema em durante o rebaleceamento.
         return properties;
     }
 
