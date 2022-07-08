be4435686a64165fa6d3c4ded0f57ff9103d6a5f
Gustavo Silva <gustavo_almeida11@hotmail.com>
2022-07-07 23:31:09 -0300

    add acks config

--- common-kafka/src/main/java/br/com/alura/ecommerce/KafkaDispatcher.java
+++ common-kafka/src/main/java/br/com/alura/ecommerce/KafkaDispatcher.java
@@ -25,6 +25,7 @@ public class KafkaDispatcher<T> implements Closeable {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092"); // Local onde o kafka tá rodando
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); // Serializador de String para bytes
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GsonSerializer.class.getName());
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
 
         return properties;
     }
